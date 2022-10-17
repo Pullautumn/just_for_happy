44467a7e4b09a3ebcd826d35b37762c2b5b5cc2a
wd210010 <76995206+wd210010@users.noreply.github.com>
2022-10-17 14:15:46 +0800

    Update qhtz.py

--- qhtz.py
+++ qhtz.py
@@ -1,32 +1,33 @@
 import requests,json ,time, datetime,hashlib
 
-# # 企业微信推送参数
-# corpid = ''
-# agentid = ''
-# corpsecret = ''
-# touser = ''
-# # 推送加 token
-# plustoken = '5c1524fa76a9420db7089decc1817f55'
+# 企业微信推送参数
+corpid = ''
+agentid = ''
+corpsecret = ''
+touser = ''
+# 推送加 token
+plustoken = '5c1524fa76a9420db7089decc1817f55'
 
 #茄皇登录https://apig.xiaoyisz.com/qiehuang/ga/public/api/login里面的请求body全部
+#不是优惠券的奖品有货会推送
 qhbody='{"appId":"wx532ecb3bdaaf92f9","openId":"oBk224nb3ruPrPfhTaRJidB4-a34","wid":3152750189,"signature":"3753808EA1D138880D410EC31430E7DC"}'
 
-
-# def Push(contents):
-#     # 微信推送
-#     if all([corpid, agentid, corpsecret, touser]):
-#         token = \
-#         requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}').json()[
-#             'access_token']
-#         json = {"touser": touser, "msgtype": "text", "agentid": agentid, "text": {"content": "茄皇库存监控\n" + contents}}
-#         resp = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={token}", json=json)
-#         print('微信推送成功' if resp.json()['errmsg'] == 'ok' else '微信推送失败')
 #
-#     if plustoken:
-#         headers = {'Content-Type': 'application/json'}
-#         json = {"token": plustoken, 'title': '茄皇库存监控', 'content': contents.replace('\n', '<br>'), "template": "json"}
-#         resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
-#         print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
+def Push(contents):
+    # 微信推送
+    if all([corpid, agentid, corpsecret, touser]):
+        token = \
+        requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}').json()[
+            'access_token']
+        json = {"touser": touser, "msgtype": "text", "agentid": agentid, "text": {"content": "茄皇库存监控\n" + contents}}
+        resp = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={token}", json=json)
+        print('微信推送成功' if resp.json()['errmsg'] == 'ok' else '微信推送失败')
+
+    if plustoken:
+        headers = {'Content-Type': 'application/json'}
+        json = {"token": plustoken, 'title': '茄皇库存监控', 'content': contents.replace('\n', '<br>'), "template": "json"}
+        resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
+        print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
 
 
 sign_url = 'https://apig.xiaoyisz.com/qiehuang/ga/public/api/login'
@@ -67,13 +68,13 @@ gift_headers={
 }
 re_2= requests.get(url=gift_url, headers=gift_headers)
 data= json.loads(re_2.text)['data']
-#
-for i in range(0, len(data)):
-    message =data[i]['name']+' 库存：'+str(data[i]['leftStock'])
-    print(message)
 
-if data[2]['leftStock']+data[3]['leftStock']+data[4]['leftStock']+data[5]['leftStock']+data[6]['leftStock']+data[7]['leftStock']+data[8]['leftStock']+data[9]['leftStock']>0:
-    Push(contents=message)
-else:
-    print('********************'+'\n'+'洗洗睡没有货，本条消息不推送！！！')
+
+for a in range(2,len(data),1):
+    if data[a]['leftStock']>0:
+        message = data[a]['name'] + ' 库存：' + str(data[a]['leftStock'])
+        Push(contents=message)
+    else:
+        print('看看'+data[a]['name']+'--库存：'+str(data[a]['leftStock'])+'  洗洗睡没有货！！！')
+
 
